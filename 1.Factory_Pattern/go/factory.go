8be2aa60bc995285c9cd6e6ca40a2c92efe0c18d
shanqi <shanqi@goldensecurity.com.cn>
2021-07-30 12:21:48 +0800

    update

--- 1.Factory_Pattern/go/factory.go
+++ 1.Factory_Pattern/go/factory.go
@@ -5,12 +5,14 @@ package factory
 import (
 	"fmt"
 )
+
 /*
 	1.创建接口
 */
 type Shape interface {
 	Draw()
 }
+
 /************/
 
 /*
@@ -18,36 +20,36 @@ type Shape interface {
 */
 // Square类
 type Square struct {
+}
 
-} 
 func (s *Square) Draw() {
 	fmt.Println("Draw a [Square].")
 }
 
 // Circle类
 type Circle struct {
-
 }
+
 func (c *Circle) Draw() {
 	fmt.Println("Draw a [Circle].")
 }
 
 // Rectangle类
 type Rectangle struct {
-
 }
+
 func (r *Rectangle) Draw() {
 	fmt.Println("Draw a [Rectangle].")
 }
+
 /************/
 
 /*
 	3.创建工厂，生成实现接口的实体类的对象
 */
-
 type ShapeFactory struct {
-
 }
+
 // 使用getShape方法获取形状类型的对象
 func (sf *ShapeFactory) getShape(shapeType string) Shape {
 	switch shapeType {
---
+++ rust/target/debug/deps/rust-6a9815fda39ab8b1.d
@@ -0,0 +1,7 @@
+C:\Users\hh\go\src\Design_Patterns\rust\target\debug\deps\rust-6a9815fda39ab8b1.rmeta: src\lib.rs
+
+C:\Users\hh\go\src\Design_Patterns\rust\target\debug\deps\librust-6a9815fda39ab8b1.rlib: src\lib.rs
+
+C:\Users\hh\go\src\Design_Patterns\rust\target\debug\deps\rust-6a9815fda39ab8b1.d: src\lib.rs
+
+src\lib.rs:
